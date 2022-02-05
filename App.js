2cda45ae1ed66aae890a420460a11deaa814876b
Markus Sibakov <markus.sibakov@myy.haaga-helia.fi>
2022-02-05 17:28:53 +0200

    laskinkomponentit navilla, kontekstilla ja propseilla

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createContext, useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Calculator from './components/Calculator';
+import History from './components/History';
+
+import { StyleContext, styles } from './StyleContext';
 
 export default function App() {
+  
+  const [history, setHistory] = useState([]);
+
+  const appendHistory = (item) => {
+    setHistory([...history, {key: item}]);
+
+  };
+
+  const Stack = createNativeStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <StyleContext.Provider value={styles}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name='Calculator'>
+            { props => <Calculator {...props} appendHistory={appendHistory}/> }
+          </Stack.Screen>
+          <Stack.Screen name='History' >
+            { props => <History {...props} history={history} />}
+          </Stack.Screen>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </StyleContext.Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
